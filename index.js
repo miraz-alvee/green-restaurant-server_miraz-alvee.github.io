afc3877f6db4a14192c9bc155819c33b70d3d7d0
Miraz Alvee <mirazalvee98@gmail.com>
2023-10-09 02:51:38 +0600

    server

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_password}@cluster0.8ibuffr.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,15 +28,42 @@ async function run() {
     
     const menuCollection = client.db("rangdhanuRestaurantDB").collection("menus");
     const reviewCollection = client.db("rangdhanuRestaurantDB").collection("reviews");
+    const cartCollection = client.db("rangdhanuRestaurantDB").collection("carts");
 
     app.get('/menus', async(req, res) =>{
         const result = await menuCollection.find().toArray();
         res.send(result);
-    })
+    });
     
     app.get('/reviews', async(req, res) =>{
         const result = await reviewCollection.find().toArray();
         res.send(result);
+    });
+
+    //cart collection
+    app.get('/carts', async(req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      if(!email){
+        res.send([]);
+      }
+      const query = { email : email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post('/carts', async(req, res) => {
+      const items = req.body;
+      console.log(items);
+      const result = await cartCollection.insertOne(items);
+      res.send(result);
+    });
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
     })
 
 
