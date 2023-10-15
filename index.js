570436fa766a8df6dc536b23f345906b8a4ebc90
Miraz Alvee <mirazalvee98@gmail.com>
2023-10-16 01:40:03 +0600

    'server'

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-//const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -9,22 +9,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const verifyJWT = (req, res, next) => {
-//   const authorization = req.headers.authorization;
-//   if (!authorization) {
-//     return res.status(401).send({ error: true, message: 'unauthorized access' });
-//   }
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
 
-//   const token = authorization.split(' ')[1];
+  const token = authorization.split(' ')[1];
 
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
-//     if (error) {
-//       return res.status(401).send({ error: true, message: 'unauthorized access' });
-//     }
-//     req.decoded = decoded;
-//     next();
-//   })
-// }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_password}@cluster0.8ibuffr.mongodb.net/?retryWrites=true&w=majority`;
@@ -48,37 +48,49 @@ async function run() {
     const cartCollection = client.db("rangdhanuRestaurantDB").collection("carts");
     const usersCollection = client.db("rangdhanuRestaurantDB").collection("users");
 
-    // app.post('/jwt', (req, res) => {
-    //   const user = req.body;
-    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-    //   res.send({ token })
-    // })
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token })
+    })
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'forbidden message' });
+      }
+      next();
+    }
 
     //users related api
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
 
-    // app.get('/users/admin/:email', verifyJWT, async (req, res) => {
-    //   const email = req.params.email;
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
 
-    //   if (req.decoded.email !== email) {
-    //     res.send({ admin: false })
-    //   }
-    //   const query = { email: email }
-    //   const user = await usersCollection.findOne(query);
-    //   const result = { admin: user?.role === 'admin' }
-    //   res.send(result);
-    // })
+      if (req.decoded.email !== email) {
+        res.send({ admin: false })
+      }
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result);
+    })
 
     app.post('/users', async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
       const existingUser = await usersCollection.findOne(query);
+
       if (existingUser) {
         return res.send({ message: 'user already exists' })
       }
+
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
@@ -95,12 +107,12 @@ async function run() {
       res.send(result);
     });
 
-    // app.delete('/users/admin/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await cartCollection.deleteOne(query);
-    //   res.send(result);
-    // });
+    app.delete('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
 
     //menu related api
     app.get('/menus', async (req, res) => {
@@ -114,18 +126,18 @@ async function run() {
     });
 
     //cart collection
-    app.get('/carts', async (req, res) => {
+    app.get('/carts', verifyJWT, async (req, res) => {
       const email = req.query.email;
       // console.log(email);
       if (!email) {
         res.send([]);
       }
 
-      // const decodedEmail = req.decoded.email;
-      // if(!decodedEmail){
-      //   return res.status(403).send({ error: true, message: 'forbidden access' });
-      // }
-      
+      const decodedEmail = req.decoded.email;
+      if (!decodedEmail) {
+        return res.status(403).send({ error: true, message: 'forbidden access' });
+      }
+
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
